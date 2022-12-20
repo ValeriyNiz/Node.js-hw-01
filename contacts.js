32a631f63b5aadaa248723f587c3cc997f91231b
ValeriyNiz <nizucer123@gmail.com>
2022-12-20 14:30:51 +0200

    start

---
+++ contacts.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+const path = require('path');
+
+const contactsPath = path.basename('./db/contacts.json')
+
+const listContacts = ()  => {
+  // ...твой код
+}
+
+const getContactById = (contactId) => {
+  // ...твой код
+}
+
+const removeContact = (contactId) => {
+  // ...твой код
+}
+
+const addContact = (name, email, phone) => {
+  // ...твой код
+}
